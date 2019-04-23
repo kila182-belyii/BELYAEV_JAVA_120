ab9506fa3197f3384d3ef202eb6e1916741a02ff
Kirill182 <kila182@list.ru>
2019-04-23 22:08:20 +0300

    Classwork_1

--- Classworks/Classwork_1/Program.java
+++ Classworks/Classwork_1/Program.java
@@ -1,25 +1,23 @@
 public class Program{
 	public static void main(String[] args) {
 		int number = 135;
-		int a = number%2;
+		int x0 = number%2;
 		number = number / 2;
-		int b = number%2;
+		int x1 = number%2;
 		number = number / 2;
-		int c = number%2;
+		int x2 = number%2;
 		number = number / 2;
-		int d = number%2;
+		int x3 = number%2;
 		number = number / 2;
-		int e = number%2;
+		int x4 = number%2;
 		number = number / 2;
-		int f = number%2;
+		int x5 = number%2;
 		number = number / 2;
-		int g = number%2;
+		int x6 = number%2;
 		number = number / 2;
-		int h = number%2;
+		int x7 = number%2;
 		number = number / 2;
-		int i = number%2;
-		number = number / 2;
-	System.out.print(a + "" + b + "" + c + "" + d + "" + e + "" + f + "" + h + "" + i);
+	System.out.print(x7 + "" + x6 + "" + x5 + "" + x4 + "" + x3 + "" + x2 + "" + x1 + "" + x0);
 
 	}
 }
