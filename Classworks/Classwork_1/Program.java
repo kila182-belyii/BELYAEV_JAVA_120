8716694d52db793ce6a047e8a19248e73302150b
Kirill182 <kila182@list.ru>
2019-04-23 19:55:05 +0300

    Classworks

---
+++ Classworks/Classwork_1/Program.java
@@ -0,0 +1,24 @@
+public class Program{
+	public static void main(String[] args) {
+		int number = 135;
+		int a = number%2;
+		int b = a%2;
+		int c = b%2;
+		int d = c%2;
+		int e = d%2;
+		int f = e%2;
+		int g = f%2;
+		int h = g%2;
+		int i = h%2;
+	System.out.print(a);
+	System.out.print(b);
+	System.out.print(c);
+	System.out.print(d);
+	System.out.print(e);
+	System.out.print(f);
+	System.out.print(g);
+	System.out.print(h);
+	System.out.print(i);
+
+	}
+}
